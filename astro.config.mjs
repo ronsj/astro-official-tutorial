38a2fc7b88fb04d89142dc73477d48b1acf65d73
Ronald San Jose <ronsj1@gmail.com>
2026-06-20 19:01:01 -0700

    Update env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || 'http://localhost:4321',
+  site: process.env.VERCEL_URL || 'http://localhost:4321',
   integrations: [react()],
 });

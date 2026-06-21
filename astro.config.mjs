e4bac9b59b1e8f1ba1dc4fc76bb9235792ce2e69
Ronald San Jose <ronsj1@gmail.com>
2026-06-20 18:41:48 -0700

    Add React and Gretting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || 'http://localhost:4321',
+  integrations: [react()],
 });
